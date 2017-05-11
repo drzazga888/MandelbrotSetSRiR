edf81a8ae50ccf64197f8a578e618692fe7ef2b9
IanRacon <rafalcosz@gmail.com>
2017-05-11 02:50:44 +0200

    Added better examples

--- parallel/examples.sh
+++ parallel/examples.sh
@@ -2,18 +2,19 @@
 
 dataDir="."
 outDataFile="data.txt"
-case_labels=("Quad_Spiral_Valley" "Double_Scepter_Valley" "Seahorse_Valley" "Mini_Mandelbrot" "Scepter_Variant" "Basic_Mandelbrot")
-xmin_limits=(0.273 -0.104 -0.75 -2 0 -2)
-xmax_limits=(0.276 -0.100 -0.74 -1.5 30 1) 
-ymin_limits=(0.484 0.835 0 -0.02 -0.007 -1)
-ymax_limits=(0.486 0.8385 0.2 0.02 0.007 1)
-step_sizes=(0.0000025 0.0000025 0.000025 0.00025 0.001 0.0008)
+case_labels=("Quad_Spiral_Valley" "Double_Scepter_Valley" "Seahorse_Valley" "Mini_Mandelbrot" "Basic_Mandelbrot")
+xmin_limits=(0.273 -0.104 -0.8 -2 -2)
+xmax_limits=(0.276 -0.100 -0.7 -1.5 1) 
+ymin_limits=(0.484 0.835 0.1 -0.02 -1)
+ymax_limits=(0.486 0.8385 0.2 0.02 1)
+step_sizes=(0.000005 0.000005 0.00025 0.0025 0.005)
 if [ ! -f /opt/nfs/mpich-3.2/bin/mpiexec ]; 
 then
 	echo "Ostrzeżenie: w ścieżce /opt/nfs/mpich-3.2/bin/ nie znaleziono poprawnego mpiexec"
-	exit
+	#exit
 fi
 run_command="/opt/nfs/mpich-3.2/bin/mpiexec -n 4 ./mandelbrot.run"
+run_command="mpiexec -n 4 ./mandelbrot.run"
 
 if [ ! -d "$dataDir" ];
 then
